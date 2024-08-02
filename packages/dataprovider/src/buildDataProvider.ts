a43f849e4d45c07000ea2729177241536de9185d
Michael Rainer <m.rainer@webware.dev>
2024-08-02 11:15:37 +0200

    update dataprovider to ra-data-graphql v5 - major change: buildDataProvider now synchronously

--- packages/dataprovider/src/buildDataProvider.ts
+++ packages/dataprovider/src/buildDataProvider.ts
@@ -21,7 +21,7 @@ export const defaultOptions: Options = {
   ...defaultOurOptions,
 };
 
-const buildDataProvider = (options: Options): Promise<DataProvider> => {
+const buildDataProvider = (options: Options): DataProvider => {
   const fullOptions = merge({}, defaultOptions, options);
   const buildQueryFactory = buildQuery(fullOptions);
   const buildOptions = merge(
@@ -32,45 +32,43 @@ const buildDataProvider = (options: Options): Promise<DataProvider> => {
     },
     fullOptions,
   );
-  return buildRaGraphqlDataProvider(buildOptions).then(
-    (graphQLDataProvider) => {
-      return {
-        ...graphQLDataProvider,
-        deleteMany: <RecordType extends RaRecord = any>(
-          resource: string,
-          params: DeleteManyParams<RecordType>,
-        ) => {
-          const { ids, ...otherParams } = params;
-          return Promise.all(
-            params.ids.map((id: string) =>
-              graphQLDataProvider.delete(resource, {
-                id,
-                ...otherParams,
-              } as DeleteParams<RecordType>),
-            ),
-          ).then((results) => {
-            return { data: results.map(({ data }: any) => data.id) };
-          });
-        },
-        updateMany: <RecordType extends RaRecord = any>(
-          resource: string,
-          params: UpdateManyParams<RecordType>,
-        ) => {
-          const { ids, ...otherParams } = params;
-          return Promise.all(
-            params.ids.map((id: string) =>
-              graphQLDataProvider.update(resource, {
-                id,
-                ...otherParams,
-              } as UpdateParams<RecordType>),
-            ),
-          ).then((results) => {
-            return { data: results.map(({ data }: any) => data.id) };
-          });
-        },
-      };
+  const graphQLDataProvider = buildRaGraphqlDataProvider(buildOptions);
+
+  return {
+    ...graphQLDataProvider,
+    deleteMany: <RecordType extends RaRecord = any>(
+      resource: string,
+      params: DeleteManyParams<RecordType>,
+    ) => {
+      const { ids, ...otherParams } = params;
+      return Promise.all(
+        params.ids.map((id: string) =>
+          graphQLDataProvider.delete(resource, {
+            id,
+            ...otherParams,
+          } as DeleteParams<RecordType>),
+        ),
+      ).then((results) => {
+        return { data: results.map(({ data }: any) => data.id) };
+      });
     },
-  );
+    updateMany: <RecordType extends RaRecord = any>(
+      resource: string,
+      params: UpdateManyParams<RecordType>,
+    ) => {
+      const { ids, ...otherParams } = params;
+      return Promise.all(
+        params.ids.map((id: string) =>
+          graphQLDataProvider.update(resource, {
+            id,
+            ...otherParams,
+          } as UpdateParams<RecordType>),
+        ),
+      ).then((results) => {
+        return { data: results.map(({ data }: any) => data.id) };
+      });
+    },
+  };
 };
 
 export default buildDataProvider;
--- packages/dataprovider/src/useDataProvider.ts
+++ packages/dataprovider/src/useDataProvider.ts
@@ -1,18 +1,9 @@
 import buildDataProvider, { defaultOurOptions } from "./buildDataProvider";
-import { useEffect, useState } from "react";
 import { DataProviderOptions } from "./types";
 import { DataProvider } from "react-admin";
 
-const useDataProvider = (options: DataProviderOptions) => {
-  const [dataProvider, setDataProvider] = useState<DataProvider>();
-
-  useEffect(() => {
-    buildDataProvider({ ...defaultOurOptions, ...options }).then((p) => {
-      setDataProvider(() => p);
-    });
-  }, []);
-
-  return dataProvider;
+const useDataProvider = (options: DataProviderOptions): DataProvider => {
+  return buildDataProvider({ ...defaultOurOptions, ...options });
 };
 
 export default useDataProvider;
